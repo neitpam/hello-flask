f912457236d9c905073a2ddc9a985852dd810cca
Neitpam <neitpam@gmail.com>
2018-10-25 16:07:34 -0500

    create format.py file to demonstrate placeholder

---
+++ format.py
@@ -0,0 +1,14 @@
+markup= """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>{0}</title>
+    </head>
+    <body>
+        <h1>{1}</h1>
+    </body>
+</html>
+"""
+
+markup = markup.format('My Page Title', 'Page Heading')
+print(markup)
