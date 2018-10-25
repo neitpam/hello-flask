621dd6334733f4fc6fb1cd21cf057a4b6cd32b88
Neitpam <neitpam@gmail.com>
2018-10-25 14:25:03 -0500

    add form to main.py file

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form = """
+<!DOCTYPE html>
+<html>
+    <body>
+        <form>
+            <label for=""first-name">First Name</label>
+            <input  id="first-name" type="text" name="first_name" />
+            <input type="submit" />
+        </form>
+    </body>
+</html>
+"""
+
 @app.route("/")
 def index():
-    return "Hello World"
+    return form
 
 app.run ()
